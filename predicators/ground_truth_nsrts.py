1ca343f014aa3aed29198635141c56adaf73b9fa
Willie McClinton <wbm3@mit.edu>
2022-08-30 16:12:20 -0400

    fixed checks

--- predicators/ground_truth_nsrts.py
+++ predicators/ground_truth_nsrts.py
@@ -3009,7 +3009,6 @@ def _get_behavior_gt_nsrts() -> Set[NSRT]:  # pragma: no cover
             raise ValueError(
                 f"Unexpected base option name: {base_option_name}")
 
-
     return nsrts
 
 
