cbecc4954c15aac9b1e21f6d919d202a0f2dcda7
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-08-31 23:30:17 -0400

    lint  + yapf

--- scripts/local/check_behavior_results.py
+++ scripts/local/check_behavior_results.py
@@ -1,8 +1,19 @@
+"""Given BEHAVIOR experiments have been run with the `run_behavior_tests`
+script, analyze the logs directory and print all tasks that have a non-zero
+success rate."""
+
 import glob
+
 import dill as pkl
 
-for filename in glob.glob("logs/*/*"):
-    with open(filename, "rb") as f:
-        data = pkl.load(f)
-    if data['results']['num_solved'] != 0:
-        print(filename)
+
+def _main() -> None:
+    for filename in glob.glob("logs/*/*"):
+        with open(filename, "rb") as f:
+            data = pkl.load(f)
+        if data['results']['num_solved'] != 0:
+            print(filename)
+
+
+if __name__ == '__main__':
+    _main()
