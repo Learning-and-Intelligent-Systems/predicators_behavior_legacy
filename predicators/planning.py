492ad5fa83c815ecc0c37b21c6feccddac5ce63a
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-10-14 12:19:31 -0400

    print domain and problem str

--- predicators/planning.py
+++ predicators/planning.py
@@ -832,6 +832,8 @@ def _sesame_plan_with_fast_downward(
         print(f"Objects: {objects}")
         env = get_or_create_env("behavior")
         print(f"Task Relevant Types: {env.task_relevant_types}")
+        print(f"Domain String: {dom_str}")
+        print(f"Problem String: {prob_str}")
         exit()
 
     assert len(num_nodes_created) == 1
