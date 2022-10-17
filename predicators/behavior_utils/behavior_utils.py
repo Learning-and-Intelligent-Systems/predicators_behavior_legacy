11ff74cd3c0a14af70f514e88dd8977d94f4c606
Willie McClinton <wbm3.important@gmail.com>
2022-10-17 16:33:33 -0400

    Added relevant objects (#68)
    * fixed
    
    * added more
    
    * fixed lint

--- predicators/behavior_utils/behavior_utils.py
+++ predicators/behavior_utils/behavior_utils.py
@@ -35,7 +35,9 @@ ALL_RELEVANT_OBJECT_TYPES = {
     'trash_can', 'backpack', 'toothbrush', 'shampoo', 'underwear', 'door',
     'window', 'toothpaste', 'package', 'highlighter', 'swivel_chair',
     'document', 'bottom_cabinet_no_top', 'folder', 'bottom_cabinet',
-    'top_cabinet', 'sofa'
+    'top_cabinet', 'sofa', 'oatmeal', 'chip', 'vegetable_oil', 'sugar',
+    'cabinet', 'floor', 'pasta', 'sauce', 'electric_refrigerator', 'olive_oil',
+    'sugar_jar', 'spaghetti_sauce', 'mayonnaise', 'fridge'
 }
 PICK_PLACE_OBJECT_TYPES = {
     'mineral_water', 'oatmeal', 'blueberry', 'headset', 'jug', 'flank',
